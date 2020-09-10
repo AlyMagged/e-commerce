700ce1f9caf02e225f574483fbd6d9c794e47de1
AlyMagged <alimaged98@gmail.com>
2020-09-10 15:32:32 +0200

    better validations for update

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -14,9 +14,11 @@ class BlogPostModelForm(forms.ModelForm):
         fields = ['title', 'slug', 'content']
 
     def clean_title(self, *args, **kwargs):
+        instance = self.instance
         title = self.cleaned_data.get('title')
         qs = BlogPost.objects.filter(title__iexact=title)
-
+        if instance is not None:
+            qs = qs.exclude(pk=instance.pk) # id = instance.id
         if qs.exists():
             raise forms.ValidationError('this title has been already used.')
         return title
