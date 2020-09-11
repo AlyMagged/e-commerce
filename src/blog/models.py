5eb3cac8288250f54011537150b7a042f117dccb
AlyMagged <alimaged98@gmail.com>
2020-09-11 02:52:31 +0200

    model managers and custom querysets

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,8 +1,26 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 User = settings.AUTH_USER_MODEL
+
+
 # Create your models here.
+
+class BlogPostQuerySet(models.QuerySet):
+    def published(self):
+        now = timezone.now()
+        return self.filter(publish_date__lte=now)
+
+
+class BlogPostManager(models.Manager):
+    def get_queryset(self):
+        return BlogPostQuerySet(self.model, using=self._db)
+
+    def published(self):
+        return self.get_queryset().published()
+
+
 class BlogPost(models.Model):
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
@@ -11,6 +29,8 @@ class BlogPost(models.Model):
     publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    objects = BlogPostManager()
 
     class Meta:
         ordering = ['-publish_date', '-timestamp', '-updated']
@@ -23,6 +43,3 @@ class BlogPost(models.Model):
 
     def get_delete_url(self):
         return f'{self.get_absolute_url()}/delete'
-
-
-
