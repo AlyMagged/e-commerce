496994cb687876f15bcae55fa2d89609f82a1551
AlyMagged <alimaged98@gmail.com>
2020-09-24 17:03:25 +0200

    user login

---
+++ src/ecommerce/views.py
@@ -0,0 +1,61 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import ContactForm, LoginForm, RegisterForm
+
+
+def home_page(request):
+    context = {
+        'title': 'Hello World!',
+        'content': 'Welcome to home page.',
+    }
+    if request.user.is_authenticated:
+        context['premium_content'] = 'a7aaaaaaaaaaaa'
+    template = 'home_page.html'
+    return render(request, template, context)
+
+
+def about_page(request):
+    context = {
+        'title': 'About Page',
+        'content': 'Welcome to about page.',
+    }
+    template = 'home_page.html'
+    return render(request, template, context)
+
+
+def contact_page(request):
+    form = ContactForm(request.POST or None)
+    context = {
+        'title': 'Contact Us',
+        'content': 'Welcome to contact page.',
+        'form': form,
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+
+    template = 'contact/view.html'
+    return render(request, template, context)
+
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        'form': form,
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+    template = 'auth/login.html'
+    return render(request, template, context)
+
+
+def register_page(request):
+    return render(request, '', {})
