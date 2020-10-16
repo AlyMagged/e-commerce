562dc2e75d56ad73143f7285575c638a9186ae4b
AlyMagged <alimaged98@gmail.com>
2020-10-16 23:31:57 +0200

    reverse for urls and navbar

--- src/products/models.py
+++ src/products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.signals import pre_save, post_save
 import random
 import os
+from django.urls import reverse
 
 from .utils import unique_slug_generator
 
@@ -61,7 +62,8 @@ class Product(models.Model):
     objects = ProductManager()
 
     def get_absolute_url(self):
-        return f'/products/{self.slug}'
+        return reverse('products:detail', kwargs={'slug': self.slug})
+        # return f'/products/{self.slug}'
 
     def __str__(self):
         return self.title
